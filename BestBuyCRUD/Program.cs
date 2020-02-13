aed98b833f8f112c7c5053cc0a0db5f7afd1949a
Andrew McMahon <andrewmcm26@gmail.com>
2020-02-13 10:23:58 -0600

    Added comments

--- BestBuyCRUD/Program.cs
+++ BestBuyCRUD/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace BestBuyCRUD
 {
@@ -6,7 +9,38 @@ namespace BestBuyCRUD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var departments = GetAllDepartments();
+
+            foreach (var dept in departments)
+            {
+                Console.WriteLine(dept);
+            }
+        }
+
+        static IEnumerable GetAllDepartments()
+        {
+            MySqlConnection conn = new MySqlConnection(); 
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt"); //Give connection a connection string
+
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT Name FROM Departments;"; // Our SQL query
+
+            using (conn)
+            {
+                conn.Open();
+                List<string> allDepartments = new List<string>();
+
+                MySqlDataReader reader = cmd.ExecuteReader(); //To read through the result of the cmd query
+
+                while (reader.Read() == true) //Means while there are more rows to read
+                {
+                    var currentDepartment = reader.GetString("Name");
+                    allDepartments.Add(currentDepartment);
+                }
+
+                return allDepartments;
+            }
         }
     }
 }
+
